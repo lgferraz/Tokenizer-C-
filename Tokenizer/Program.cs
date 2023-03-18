706c544333f5d1ad833f88e35a96a7008244a8ce
Lucas Gabriel <31184517+lgferraz@users.noreply.github.com>
2023-03-18 04:25:43 -0300

    Adicionar arquivos de projeto.

---
+++ Tokenizer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tokenizer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //entrada de teste
+            string entrada = "teste tokenizer= e ponto;{foda}&&m,&";
+            //separadores
+            string[] separadores = new string[] { "=", "{", "}", "(", ")", ";", ",", "." };
+            //instancia o tokenizer
+            Tokenizer obj = new Tokenizer();
+            //tolkeniza
+            obj.Tokenizar(entrada, separadores);
+            //coloca tolkens numa lista e mostra o resultado
+            string[] saida = obj.Tolkens();
+            Console.WriteLine("quantidade de tolkens: "+saida.Length);
+            Console.Write("tolkens: ");
+            foreach (string s in saida)
+            {
+                Console.Write($"'{s}'");
+            }
+            Console.ReadKey();
+        }
+    }
+}
